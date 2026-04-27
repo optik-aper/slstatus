6171cae6b08dd497e1a2ddd10c3cad8e15b0aba0
Michael Riley <michael@tsuga.dev>
2026-04-27 11:25:18 -0400

    Pad cpu & ram with extra space

--- config.def.h
+++ config.def.h
@@ -71,8 +71,8 @@ static const struct arg args[] = {
 	{ run_command, " up: %s |", "awk '{printf(\"%d:%02d:%02d:%02d\",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}' /proc/uptime" },
 	{ disk_free, " / %s |", "/" },
 	{ disk_free, " ~ %s |", "/home/michael/" },
-	{ cpu_perc, " cpu: %s%% |" },
-	{ ram_perc, " ram: %s%% |" },
+	{ cpu_perc, " cpu: %2s%% |" },
+	{ ram_perc, " ram: %2s%% |" },
 	{ battery_perc, " bat: %s%% |", "BAT0" },
 	{ datetime, " %s", "%a %B %d %T" },
 };
